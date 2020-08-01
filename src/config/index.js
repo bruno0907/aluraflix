bad367dea0fb008d6a79b940aa02a8a80f975ea4
Bruno Mariani <bruno0907@gmail.com>
2020-08-01 13:21:27 -0300

    Fixed scripts

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL = window.location.hostname.includes('localhost')
-    ? 'http://localhost:3333'
+    ? 'https://aluraflix-backend.herokuapp.com/'
     : 'http://localhost:3333';
 
 export default {
